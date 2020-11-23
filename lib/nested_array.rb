e4f82d2a2108282ca131507ee514582540597c03
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-23 18:05:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,7 +28,6 @@ end
 
 def matrix_lookup(matrix, row, column)
 matrix[row][column]
-
 end
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
